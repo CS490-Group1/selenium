50a75e9c83f2a495d1bc3ce775b28cdd92185889
jv456njit <jv456@njit.edu>
2024-05-04 23:23:17 -0400

    Final selenium tests

--- 15_admin_create_man_tech.py
+++ 15_admin_create_man_tech.py
@@ -25,41 +25,77 @@ admin_panel_button.click()
 time.sleep(1) #wait for things to load
 
 fname="Selenium"
-lname="Manager"
+lman="Manager"
 
 # Fill out the registration form
-first_name = driver.find_element(By.ID, 'man_first_name')
-first_name.send_keys(fname)
+man_first_name = driver.find_element(By.ID, 'man_first_name')
+man_first_name.send_keys(fname)
 time.sleep(0.75)   
 
-last_name = driver.find_element(By.ID, 'man_last_name')
-last_name.send_keys(lname)
+man_last_name = driver.find_element(By.ID, 'man_last_name')
+man_last_name.send_keys(lman)
 time.sleep(0.75)
 
-dob = driver.find_element(By.ID, 'man_dob')
-dob.send_keys('01/01/1995')
-dob.send_keys(Keys.ESCAPE)
+man_dob = driver.find_element(By.ID, 'man_dob')
+man_dob.send_keys('01/01/1995')
+man_dob.send_keys(Keys.ESCAPE)
 time.sleep(0.75)
 
-email = driver.find_element(By.ID, 'man_email')
-email.send_keys(f"{fname+lname}@gmail.com")
+man_email = driver.find_element(By.ID, 'man_email')
+man_email.send_keys(f"{fname+lman}@gmail.com")
 time.sleep(0.75)
 
-password = driver.find_element(By.ID, 'man_password')
-password.send_keys('Admin123')
+man_password = driver.find_element(By.ID, 'man_password')
+man_password.send_keys('Admin123')
 time.sleep(0.75)
 
-confirm_password = driver.find_element(By.ID, 'man_confirm_password')
-confirm_password.send_keys('Admin123')
+man_confirm_password = driver.find_element(By.ID, 'man_confirm_password')
+man_confirm_password.send_keys('Admin123')
 time.sleep(0.75)
 
-phone = driver.find_element(By.ID, 'man_phone')
-phone.send_keys('123-456-7890')
+man_phone = driver.find_element(By.ID, 'man_phone')
+man_phone.send_keys('123-456-7890')
 time.sleep(0.75)
 
 # Click the 'Create New Account' button to submit the form
 create_man_button = driver.find_element(By.CSS_SELECTOR, '.registerBtn')
 create_man_button.click()
+
+time.sleep(8)  # Wait for 5 seconds to see the changes
+
+tech_first_name = driver.find_element(By.ID, 'tech_first_name')
+tech_first_name.send_keys(fname)
+time.sleep(0.75)   
+
+ltech="Technician"
+tech_last_name = driver.find_element(By.ID, 'tech_last_name')
+tech_last_name.send_keys(ltech)
+time.sleep(0.75)
+
+tech_dob = driver.find_element(By.ID, 'tech_dob')
+tech_dob.send_keys('01/01/1995')
+tech_dob.send_keys(Keys.ESCAPE)
+time.sleep(0.75)
+
+tech_email = driver.find_element(By.ID, 'tech_email')
+tech_email.send_keys(f"{fname+ltech}@gmail.com")
+time.sleep(0.75)
+
+tech_password = driver.find_element(By.ID, 'tech_password')
+tech_password.send_keys('Admin123')
+time.sleep(0.75)
+
+tech_confirm_password = driver.find_element(By.ID, 'tech_confirm_password')
+tech_confirm_password.send_keys('Admin123')
+time.sleep(0.75)
+
+tech_phone = driver.find_element(By.ID, 'tech_phone')
+tech_phone.send_keys('123-456-7890')
+time.sleep(0.75)
+
+# Click the 'Create New Account' button to submit the form
+create_tech_button = driver.find_element(By.NAME, 'techCreateButton')
+create_tech_button.click()
 
 time.sleep(8)  # Wait for 5 seconds to see the changes
 
